32497d7f0d1ba7e90de1aec9d154344dd45575ed
丁敬 <dingjing@live.cn>
2017-11-22 21:05:27 +0800

    log

---
+++ src/tool_log.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+> FileName: tool_log.h
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Wed 22 Nov 2017 06:59:34 PM DST
+ ************************************************************************/
+
+#ifndef _TOOL_LOG_H
+#define _TOOL_LOG_H
+#endif
